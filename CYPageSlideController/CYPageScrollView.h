1cb9c094f55862d05c69bc2e246c83f6cad2797a
chenyiliang <yiliang.chen1318@hotmail.com>
2019-01-13 22:26:20 +0800

    add CYPageScrollView

---
+++ CYPageSlideController/CYPageScrollView.h
@@ -0,0 +1,17 @@
+//
+//  CYGestureScrollView.h
+//  PageSlideControllerDemo
+//
+//  Created by Chen Yiliang on 23/10/2017.
+//  Copyright © 2017 Chen Yiliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+IB_DESIGNABLE
+
+@interface CYPageScrollView : UIScrollView
+
+@property (nonatomic, assign) IBInspectable BOOL alwaysBounceHorizontalLeft;
+
+@end
